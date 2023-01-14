8da18030d880f84a29e3cf040f5e10e4089d6045
joseph <luis10amador@gmail.com>
2023-01-13 18:30:11 -0600

    Seccion 2 - Pasar datos a la vista

--- src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
@@ -1,12 +1,18 @@
 package com.bolsadeideas.springboot.web.app.controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @Controller
+@RequestMapping("/app")
 public class IndexController {
-    @GetMapping({"/index","/","/home"})
-    public String index() {
+    @GetMapping({"/index", "/home", "/"})
+    public String index(Model model) {
+        model.addAttribute("titulo", "Hola Spring framework con Model!");
         return "index";
     }
 }
